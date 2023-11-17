9d21a176c10bee415d8717e02dcb8cd210240e64
Rekoshe <42325581+Rekoshe@users.noreply.github.com>
2023-11-17 22:06:15 +0100

    trying to fix a bug that only appears on safari

--- app/reviewCard.tsx
+++ app/reviewCard.tsx
@@ -14,14 +14,14 @@ export default function ReviewCard(props: {
 
 
     return (
-        <div className={styles.reviewCardCont} style={{ borderRadius: props.carWidth * 0.07, padding: props.carWidth * 0.04, margin: props.carWidth * 0.01}}>
+        <div className={styles.reviewCardCont} style={{ borderRadius: props.carWidth * 0.07, WebkitBorderRadius: props.carWidth * 0.07, padding: props.carWidth * 0.04, margin: props.carWidth * 0.01, WebkitAppearance: 'none'}}>
             <div style={{ fontSize: props.carWidth * 0.06 }}>{props.title}</div>
             <FontAwesomeIcon style={{ fontSize: props.carWidth * 0.05 }} className={styles.reviewQoute} icon={faQuoteLeft}></FontAwesomeIcon>
             <div style={{ fontSize: props.carWidth * 0.035 }} className={styles.reviewPara}>{props.children}</div>
             <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: props.carWidth * 0.03 }}>
                 <div style={{display: 'flex', width: '80%'}}>
                     <div className={styles.reviewImage}>
-                        <Image src={props.icon} alt='review Icon' fill style={{ objectFit: 'contain', borderRadius: '100%' }}></Image>
+                        <Image src={props.icon} alt='review Icon' fill style={{ objectFit: 'contain', borderRadius: '100%', WebkitBorderRadius: '100%', WebkitAppearance: 'none' }}></Image>
                     </div>
                     <div style={{marginLeft:'0.5rem'}}>
                         <div style={{ fontSize: props.carWidth * 0.05 }}>{props.reviewerName}</div>
